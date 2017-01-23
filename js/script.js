39e785082ffd7937589efc9143b8217754c2e310
alexgooz <alexgooz92fs@gmail.com>
2017-01-23 13:44:45 -0800

    Start This is what i am starting out with on this weather app.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/* sample code
+      var weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&APPID=36392536f4144190680fc96bd357451d&callback=JSON_CALLBACK";
+
+      $.getJSON(weatherURL).done(function(data){
+     $('#currTemp').html("current temp: " + data.main.temp);*/
