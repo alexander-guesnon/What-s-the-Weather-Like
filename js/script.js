e5c96d49c373d434f721a097e0ff2ef5b33afda4
alexgooz <alexgooz92fs@gmail.com>
2017-01-24 04:14:48 -0800

    got location working
    it now can find your current location and relay it to a url
    
    i don't really want to have a manual select for the location so
    if the don't agree to let me see their location it wont work.
    
    TODO: i may come back eventually and add that functionality but idk.

--- js/script.js
+++ js/script.js
@@ -42,3 +42,21 @@ JSON_CALLBACK({
 });
 http://openweathermap.org/current
 */
+
+//location lat and long
+
+//icons http://openweathermap.org/img/w/10d.png
+
+
+//start of code
+var weatherAJAXurl="";
+
+if (navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(function(position) {
+weatherAJAXurl =  "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=67150c62fa359ac0ec0e60b78e858a9d&callback=JSON_CALLBACK";
+console.log(weatherAJAXurl);
+
+});
+}else{
+  //tell user to please turn on location in order to make that app work
+}
