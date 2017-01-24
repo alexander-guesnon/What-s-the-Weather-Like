eac6a6640b79cb42f1fcabf4ac41137f49e922bd
alexgooz <alexgooz92fs@gmail.com>
2017-01-24 06:46:36 -0800

    most of the main functional code is done
    I have arranged the data in the given JSON from the open weather map
    so that it goes in its proper div
    
    i have also implemented the switch between f to c and c to f

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-//icons http://openweathermap.org/img/w/10d.png
-
-
-//start of code
 var weatherAJAXurl="";
 var appToken = "67150c62fa359ac0ec0e60b78e858a9d";
+var tempF=0;
+var tempC=0;
+var currenttempC=true;
 
 if (navigator.geolocation) {
 navigator.geolocation.getCurrentPosition(function(position) {
-weatherAJAXurl =  "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid="+ appToken +"&callback=JSON_CALLBACK";
+weatherAJAXurl =  "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&units=metric&appid="+ appToken +"&callback=JSON_CALLBACK";
 $.ajax({
   type:"GET",
   url: weatherAJAXurl,
@@ -21,5 +20,23 @@ $.ajax({
 }
 
 function JSON_CALLBACK(data){
+  $("#weather-icon").html("<img src=\"http://openweathermap.org/img/w/"+ data.weather[0].icon + ".png\" alt=\""+ data.weather[0].main + "\"></img>");
+  tempF=((data.main.temp/ 0.5556)+32).toFixed(2);// &#176F
+  tempC=data.main.temp.toFixed(2);// &#176C
+  $("#temperature").html(tempC + " &#176C");
+  $("#current-location").html(data.name);
+  $("#sky").html("sky: "+data.weather[0].description);
+  $("#wind-speed").html(" wind-speed: " + data.wind.speed+" m/s");
   console.log(data);
 }
+$(document).ready(function(){
+  $("#temperature").click(function() {
+    if(currenttempC===true){
+      currenttempC=false;
+      $("#temperature").html(tempF + " &#176F");
+    }else{
+      currenttempC=true;
+        $("#temperature").html(tempC + " &#176C");
+    }
+  });
+});
