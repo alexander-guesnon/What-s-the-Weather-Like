763c90130889ccaa5c8855d0515c9065f9542ce8
alexgooz <alexgooz92fs@gmail.com>
2017-01-25 10:53:32 -0800

    starting on CSS
    I put in CSS to make it look close to the Inkscape render.
    also, i found an awesome icon library modeled after font awsome.
    
    https://erikflowers.github.io/weather-icons/
    
    cdn: https://cdnjs.com/libraries/weather-icons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 var weatherAJAXurl="";
 var appToken = "67150c62fa359ac0ec0e60b78e858a9d";
 var tempF=0;
@@ -7,6 +8,8 @@ var currenttempC=true;
 if (navigator.geolocation) {
 navigator.geolocation.getCurrentPosition(function(position) {
 weatherAJAXurl =  "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&units=metric&appid="+ appToken +"&callback=JSON_CALLBACK";
+//console.log(position.coords.latitude);
+//console.log(position.coords.longitude);
 $.ajax({
   type:"GET",
   url: weatherAJAXurl,
@@ -20,23 +23,23 @@ $.ajax({
 }
 
 function JSON_CALLBACK(data){
-  $("#weather-icon").html("<img src=\"http://openweathermap.org/img/w/"+ data.weather[0].icon + ".png\" alt=\""+ data.weather[0].main + "\"></img>");
+  //$("#weather-icon").html("<img src=\"http://openweathermap.org/img/w/"+ data.weather[0].icon + ".png\" alt=\""+ data.weather[0].main + "\"></img>");
   tempF=((data.main.temp/ 0.5556)+32).toFixed(2);
   tempC=data.main.temp.toFixed(2);
-  $("#temperature").html(tempC + " &#176C");
+  $("#temperature").html( "<i class=\"wi wi-thermometer\"></i> " + tempC + " &#176C");
   $("#current-location").html(data.name);
   $("#sky").html("sky: "+data.weather[0].description);
-  $("#wind-speed").html(" wind-speed: " + data.wind.speed+" m/s");
+  $("#wind-speed").html("<i class=\"wi wi-sandstorm\"></i> " + data.wind.speed+" m/s");
   console.log(data);
 }
 $(document).ready(function(){
   $("#temperature").click(function() {
     if(currenttempC===true){
       currenttempC=false;
-      $("#temperature").html(tempF + " &#176F");
+      $("#temperature").html( "<i class=\"wi wi-thermometer\"></i> " +tempF + " &#176F");
     }else{
       currenttempC=true;
-        $("#temperature").html(tempC + " &#176C");
+        $("#temperature").html( "<i class=\"wi wi-thermometer\"></i> " + tempC + " &#176C");
     }
   });
 });
