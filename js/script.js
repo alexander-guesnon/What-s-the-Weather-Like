71325cd3b4c5887bafe14fc906b76891cc35fee9
alexgooz <alexgooz92fs@gmail.com>
2017-01-26 00:14:49 -0800

    all user requirements are finished
    I have finished the images selector for the types of weather
    this fulfilled the code cape user story. now all I need to do is
    to polish up and post.

--- js/script.js
+++ js/script.js
@@ -23,13 +23,58 @@ $.ajax({
 }
 
 function JSON_CALLBACK(data){
-  //$("#weather-icon").html("<img src=\"http://openweathermap.org/img/w/"+ data.weather[0].icon + ".png\" alt=\""+ data.weather[0].main + "\"></img>");
+  //$("#weather-icon").html("<img src=\"http://openweathermap.org/img/w/"+  + ".png\" alt=\""+ data.weather[0].main + "\"></img>");
   tempF=((data.main.temp/ 0.5556)+32).toFixed(2);
   tempC=data.main.temp.toFixed(2);
   $("#temperature").html( "<i class=\"wi wi-thermometer\"></i> " + tempC + " &#176C");
   $("#current-location").html(data.name);
   $("#sky").html(data.weather[0].description);
   $("#wind-speed").html("<i class=\"wi wi-sandstorm\"></i> " + data.wind.speed+" m/s");
+  console.log(data.weather[0].icon);
+  switch(data.weather[0].icon){
+    case "01d.png"://sun
+    case "02d.png":{
+$("body").css({"background-image": "url(\"images/background_img_3.jpg\")"});
+      break;
+    }
+    case "01n"://moon
+    case "02n":{
+$("body").css({"background-image": "url(\"images/background_img_7.jpg\")"});
+      break;
+    }
+    case "03n":
+    case "04n":
+    case "03d"://cloud
+    case "04d":{
+      $("body").css({"background-image": "url(\"images/background_img_2.jpg\")"});
+      break;
+    }
+    case "09n":
+    case "10n":
+    case "09d"://rain
+    case "10d":{
+      $("body").css({"background-image": "url(\"images/background_img_1.jpg\")"});
+      break;
+    }
+    case "11d"://thunder
+    case "11d":{
+      $("body").css({"background-image": "url(\"images/background_img_5.jpg\")"});
+      break;
+    }
+    case "13n"://snow
+    case "13d":{
+      $("body").css({"background-image": "url(\"images/background_img_6.jpg\")"});
+      break;
+    }
+    case "50d"://mist
+    case "50d":{
+      $("body").css({"background-image": "url(\"images/background_img_4.jpg\")"});
+      break;
+    }
+    default:{
+
+    }
+  }
   console.log(data);
 }
 $(document).ready(function(){
